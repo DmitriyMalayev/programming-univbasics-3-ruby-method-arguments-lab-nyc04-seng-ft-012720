be8e5cbc0eb8958e937eb70c3615d545d870180f
Dmitriy Malayev <dmitriy.malayev@gmail.com>
2020-01-10 06:01:54 +0000

    Automatically backed up by Learn

--- introductions.rb
+++ introductions.rb
@@ -1,7 +1,14 @@
 # You'll be coding your methods in lib/introduction.rb.
 
-def introduction()
-  
+def introduction(name)
+  puts "Hi, my name is #{name}"
+end 
+
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}"
+
+def introduction_with_language_optional(name, language = ruby)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}"
 
 # Define a Method That Takes in and Uses an Argument
 # Define a method called introduction that takes in the argument name and outputs the phrase: "Hi, my name is #{name}."
