4a4534ff8f16cc0cba7b16cf2c19b478fe8936ba
Dmitriy Malayev <dmitriy.malayev@gmail.com>
2020-01-10 06:34:55 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -15,13 +15,13 @@ print introduction("Josh")
 # introduction("name") 
 
 def introduction_with_language(name, language)
-  puts "Hi, my name is #{name} and I am learning to program in #{language}"
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
 # introduction_with_language("name", "language")
 
 def introduction_with_language_optional(name, language = ruby)
-  puts "Hi, my name is #{name} and I am learning to program in #{language}"
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end 
 
 # introduction_with_language_optional("name", "language")
